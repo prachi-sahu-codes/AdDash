f5df462d20c522c8b18fffa43bd0fba1304f467f
prachi-sahu-codes <prachisahu998@gmail.com>
2023-10-20 19:54:33 +0530

    style: layout style added

--- src/layout/layoutContainer/LayoutContainer.jsx
+++ src/layout/layoutContainer/LayoutContainer.jsx
@@ -5,7 +5,7 @@ const LayoutContainer = ({ children }) => {
   return (
     <div>
       <NavBar />
-      <div className="bg-bg-body h-body-height">{children}</div>
+      <div className="bg-bg-body h-body-height p-7">{children}</div>
     </div>
   );
 };
