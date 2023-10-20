a62ea5ff274d66027dc0f20d105385e90a4ea4c9
prachi-sahu-codes <prachisahu998@gmail.com>
2023-10-20 17:57:31 +0530

    chore: dummy data added

---
+++ src/data/data.js
@@ -0,0 +1,71 @@
+export const graphData = [
+  {
+    id: 1,
+    group: "Male",
+    clicks: 135,
+    cost: 5421,
+    conversions: 17,
+    revenue: 21345,
+  },
+  {
+    id: 2,
+    group: "Female",
+    clicks: 256,
+    cost: 9648,
+    conversions: 29,
+    revenue: 32198,
+  },
+  {
+    id: 3,
+    group: "Unknown",
+    clicks: 178,
+    cost: 2985,
+    conversions: 8,
+    revenue: 10432,
+  },
+];
+
+export const tableData = [
+  {
+    campaigns: "Cosmetics",
+    clicks: 712,
+    cost: 4272,
+    conversions: 8,
+    revenue: 16568,
+  },
+  {
+    campaigns: "Serums",
+    clicks: 3961,
+    cost: 27331,
+    conversions: 115,
+    revenue: 362526,
+  },
+  {
+    campaigns: "Facewash",
+    clicks: 9462,
+    cost: 76831,
+    conversions: 123,
+    revenue: 266800,
+  },
+  {
+    campaigns: "Shampoos",
+    clicks: 439,
+    cost: 2151,
+    conversions: 5,
+    revenue: 11029,
+  },
+  {
+    campaigns: "Conditioners",
+    clicks: 1680,
+    cost: 3864,
+    conversions: 49,
+    revenue: 175245,
+  },
+  {
+    campaigns: "FaceWash 2",
+    clicks: 4978,
+    cost: 29370,
+    conversions: 189,
+    revenue: 623106,
+  },
+];
